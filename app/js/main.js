439a771162abe816103e1d8b64595be1285797c6
dariaromanenko <spavak.darya.v@gmail.com>
2019-03-29 21:00:14 +0200

    update app

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    // $('.menu__btn').click(function(){
+    //     $('.menu ul').slideToggle();
+    // });
+
+    // $('.menu__btn').click(function() {
+    //     $('.menu__list').toggleClass('shown');
+    // });
+
+
+    $('.slider').slick({
+      dots: false,
+       prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    });
+
+
+});
